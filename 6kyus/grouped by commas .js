80873e9ec25006a519788c63da985f7061f62636
Kelwilson <kelwilson_achienu@icloud.com>
2024-10-17 08:14:48 -0700

    Add grouped by three solutions

---
+++ 6kyus/grouped by commas .js
@@ -0,0 +1,16 @@
+//Description:
+// Finish the solution so that it takes an input n (integer) and returns a string that is the decimal representation of the number grouped by commas after every 3 digits.
+
+// Assume: 0 <= n <= 2147483647
+
+
+//Solution:
+function groupByCommas(n) {
+    //prep [Parameters, Returns, Examples, Psuedo-codes]
+    //takes in an integer
+    // convert integer to string
+    //return the decimal representation of the number group by commas after every 3 digits
+     return n.toLocaleString('en-US');
+  //   console.log(test)
+    
+  }
