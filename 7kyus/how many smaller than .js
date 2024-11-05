800fa16678538cc5b361e01344ab3d8414c25f98
Kelwilson <kelwilson_achienu@icloud.com>
2024-11-05 03:17:31 -0800

    Added how many smaller than me solutions

---
+++ 7kyus/how many smaller than .js
@@ -0,0 +1,15 @@
+// Description :
+
+// Write a function that given, an array arr, returns an array containing at each index i the amount of numbers that are smaller than arr[i] to the right.
+
+// For example:
+
+// * Input [5, 4, 3, 2, 1] => Output [4, 3, 2, 1, 0]
+// * Input [1, 2, 0] => Output [1, 1, 0]
+
+
+function smaller(nums) {
+    return nums.map((n, i) => {
+      return nums.slice(i).filter(v => v < n).length
+    });
+   }
