75e9cddd268ca7a9fa844048e2b4758f41dc4854
Kelwilson <kelwilson_achienu@icloud.com>
2024-11-20 18:51:01 -0800

    Added removing duplicates from list solution

---
+++ 8kyu/remove duplicates from list .js
@@ -0,0 +1,28 @@
+// Duplicates:
+// Define a function that removes duplicates from an array of non negative numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// Examples:
+
+// Input -> Output
+// [1, 1, 2] -> [1, 2]
+// [1, 2, 1, 1, 3, 2] -> [1, 2, 3]
+
+
+// Solution: 
+function distinct(a) {
+    // PREP [Parameters, Returns, Examples, Psuedo-codes]
+        // Parameters:
+            // always takes in an array of positive numbers
+            // remove duplicates & return new array
+    
+        // Psuedo-codes:
+            // create a new array
+            // loop through array if current !== next push current into new array
+            // return new array
+    return [...new Set(a)]
+    
+  }
+        // Returns :
+            distinct([1,1,2]) //-> Examples = [1,2]
