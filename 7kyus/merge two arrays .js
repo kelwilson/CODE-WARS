70b4a659160573183a4857aa23f75f4f5f663145
Kelwilson <kelwilson_achienu@icloud.com>
2025-02-03 18:25:22 -0800

    Update solution

--- 7kyus/merge two arrays .js
+++ 7kyus/merge two arrays .js
@@ -56,3 +56,21 @@ function mergeArrays(a, b) {
     return a.length ? [a[0], ...mergeArrays(b, a.slice(1))] : b;
   }
   
+//   Solution 3 :
+
+function mergeArrays(a, b) {
+    // your code here
+    var returnArray = [];
+    var counter = 0;
+    while (a[counter] || b[counter] ){
+      if(a[counter]){
+        returnArray.push(a[counter]);
+      }
+      if(b[counter]){
+        returnArray.push(b[counter]);
+      }
+      counter++;
+      
+     }
+     return returnArray;
+  }
