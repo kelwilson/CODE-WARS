4a2f54de3309bd8a9fa8f3b6c0d2d91000226d69
Kelwilson <kelwilson_achienu@icloud.com>
2025-04-04 01:16:05 -0700

    Add solution bartender two

--- 8kyu/bartender drinks .js
+++ 8kyu/bartender drinks .js
@@ -74,3 +74,13 @@ function getDrinkByProfession(param){
 
 //   Solution 3:
 
+const drinks = {
+    "jabroni": "Patron Tequila",
+    "school counselor": "Anything with Alcohol",
+    "programmer": "Hipster Craft Beer",
+    "bike gang member": "Moonshine",
+    "politician": "Your tax dollars",
+    "rapper": "Cristal"
+  }
+  
+  const getDrinkByProfession = profession => drinks[profession.toLowerCase()] || "Beer"
